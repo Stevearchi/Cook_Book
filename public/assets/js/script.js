42e04906df7d3bd3059f3fa3adfbb5fa9ccf10fb
Surface <da.ga.mo.94@gmail.com>
2019-07-20 02:53:02 -0600

    added results display alternative to index.ejs

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -8,7 +8,7 @@ $("#search-button").on("click", function (event) {
     errorDiv.empty();
     console.log("Button Works!!!")
     var search = $("#searchQuery").val().trim();
-    if(search !== ""){
+    if (search !== "") {
         $("#pheader").empty();
         $("#tableBody").empty();
 
@@ -19,7 +19,7 @@ $("#search-button").on("click", function (event) {
         $("#tableBody").empty();
         errorDiv.append(errorH3 + "Please type a recipe.</h3>");
     }
-     
+
 });
 
 function showResults(search) {
@@ -39,6 +39,7 @@ function showResults(search) {
         if (results.length > 0) {
             $("#pheader").append(newHeader);
 
+            addCols(results);
             for (var i = 0; i < results.length; i++) {
                 var recipeName = results[i].recipe_name;
                 var authorName = results[i].author_name;
@@ -54,10 +55,11 @@ function showResults(search) {
                     $("<td>").html("<img id='recipeImage' class='recipeImg' src=" + image + ">"),
                 )
                 newRow.attr("data-name", results[i].id)
-                
+
                 $("#tableBody").append(newRow);
+
             }
-        }else{
+        } else {
             return errorDiv.append(errorH3 + "Sorry, no matching recipes were found.</h3>");
         }
 
@@ -65,24 +67,28 @@ function showResults(search) {
     });
 }
 
-var addCols = function (num){
-    for (var i=1;i<=num;i++) {
-        var myCol = $('<div class="col-sm-3 col-md-3 pb-2"></div>');
-        var myPanel = $('<div class="card card-outline-info" id="'+i+'Panel"><div class="card-block"><div class="card-title"><span>Card #'+i+'</span><button type="button" class="close" data-target="#'+i+'Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>Some text in '+i+' </p><img src="//placehold.it/50/eeeeee" class="rounded rounded-circle"></div></div>');
+var addCols = function (arr) {
+    for (var i = 0; i < arr.length; i++) {
+        var recipeId = arr[i].id;
+        var recipeName = arr[i].recipe_name;
+        var authorName = arr[i].author_name;
+        var ingredients = arr[i].ingredients;
+        var directions = arr[i].directions;
+        var image = arr[i].image;
+
+        var myCol = $('<div class="col-sm-6 col-md-4 pb-2"></div>');
+        // var myPanel = $('<div class="card card-outline-info" id="' + i + 'Panel"><div class="card-block"><div class="card-title"><span class="movecenter">' + recipeName + '</span><button type="button" class="close" data-target="#' + i + 'Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p> ' + authorName + ' </p><img src="' + image +'" class="mx-auto rounded rounded-circle recipeImg"></div></div>');
+        var myPanel = $('<div class="card recipe-card" id="' + i + 'Panel" style="width: 18rem;"><img class="card-img-top" src="'+ image + '" alt="Card image cap"> <div class="card-body text-center"> <h5 class="card-title">' + recipeName + '</h5> <p class="card-text">Author: ' + authorName + '</p> <a href="/recipes/indRecipe/'+ recipeId +'" class="btn btn-primary">Go to full recipe</a></div></div>');
         myPanel.appendTo(myCol);
         myCol.appendTo('#contentPanel');
     }
-    
-    
-    $('.close').on('click', function(e){
-      e.stopPropagation();  
-          var $target = $(this).parents('.col-sm-3');
-          $target.hide('slow', function(){ $target.remove(); });
+
+
+    $('.close').on('click', function (e) {
+        e.stopPropagation();
+        var $target = $(this).parents('.col-sm-3');
+        $target.hide('slow', function () { $target.remove(); });
     });
 };
 
-$('#btnGen').click(function(){
-    addCols($('#numPanels').val());
-    return false;
-});
 
