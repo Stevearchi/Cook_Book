192297c2b388ae75567eda23ff2a60f1917a5571
Surface <da.ga.mo.94@gmail.com>
2019-07-20 01:44:36 -0600

    added validation to search input, moved navbar to layout.ejs

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,25 @@
+// GLOBAL VARS
+var errorDiv = $("#err-message");
+var errorH3 = "<br><h3 class='text-center text-light'>";
+
 // Search Results Dynamic Click Events
 $("#search-button").on("click", function (event) {
     event.preventDefault();
+    errorDiv.empty();
     console.log("Button Works!!!")
-
     var search = $("#searchQuery").val().trim();
-    showResults(search);
+    if(search !== ""){
+        $("#pheader").empty();
+        $("#tableBody").empty();
+
+        showResults(search);
+        $("#searchQuery").val("");
+    } else {
+        $("#pheader").empty();
+        $("#tableBody").empty();
+        errorDiv.append(errorH3 + "Please type a recipe.</h3>");
+    }
+     
 });
 
 function showResults(search) {
@@ -42,8 +57,32 @@ function showResults(search) {
                 
                 $("#tableBody").append(newRow);
             }
+        }else{
+            return errorDiv.append(errorH3 + "Sorry, no matching recipes were found.</h3>");
         }
 
+
+    });
+}
+
+var addCols = function (num){
+    for (var i=1;i<=num;i++) {
+        var myCol = $('<div class="col-sm-3 col-md-3 pb-2"></div>');
+        var myPanel = $('<div class="card card-outline-info" id="'+i+'Panel"><div class="card-block"><div class="card-title"><span>Card #'+i+'</span><button type="button" class="close" data-target="#'+i+'Panel" data-dismiss="alert"><span class="float-right"><i class="fa fa-remove"></i></span></button></div><p>Some text in '+i+' </p><img src="//placehold.it/50/eeeeee" class="rounded rounded-circle"></div></div>');
+        myPanel.appendTo(myCol);
+        myCol.appendTo('#contentPanel');
+    }
+    
+    
+    $('.close').on('click', function(e){
+      e.stopPropagation();  
+          var $target = $(this).parents('.col-sm-3');
+          $target.hide('slow', function(){ $target.remove(); });
     });
 };
+
+$('#btnGen').click(function(){
+    addCols($('#numPanels').val());
+    return false;
+});
 
